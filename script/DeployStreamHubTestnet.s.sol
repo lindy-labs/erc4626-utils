0639739ebdbb5536129df3c552c455768d2caac3
brkomir <nenad.milenkovic154@outlook.com>
2023-12-07 14:06:12 +0100

    update testnet deploy script

--- script/DeployStreamHubTestnet.s.sol
+++ script/DeployStreamHubTestnet.s.sol
@@ -5,28 +5,21 @@ import {ERC20Mock} from "openzeppelin-contracts/mocks/ERC20Mock.sol";
 import {ERC4626Mock} from "openzeppelin-contracts/mocks/ERC4626Mock.sol";
 import {CREATE3Script} from "./base/CREATE3Script.sol";
 import {ERC4626StreamHub} from "../src/ERC4626StreamHub.sol";
+import {RemovableAssetsERC4626Mock} from "../test/mock/RemovableAssetsERC4626Mock.sol";
 
-contract DeployScript is CREATE3Script {
+contract DeployScriptTestnet is CREATE3Script {
     // constructor() CREATE3Script(vm.envString("VERSION")) {}
     constructor() CREATE3Script("1.0.0") {}
 
+    // sepolia addresses
     address constant erc20Mock = 0xB96CcC02290102424A10795bea3Ba0dF99374CD7;
-    address constant erc4626Mock = 0x415849Ed40dFd67c9422686F9E2FfF9E3d7e04D1;
+    address constant erc4626Mock = 0xF78Ab707bdC48F6E7eAC9115d3BAC2a6323C26A6;
 
     function run() external returns (ERC4626StreamHub streamHub) {
         uint256 deployerPrivateKey = uint256(vm.envBytes32("PRIVATE_KEY"));
         vm.startBroadcast(vm.addr(deployerPrivateKey));
 
-        // vm.startBroadcast(address(this));
-
-        streamHub = new ERC4626StreamHub(ERC4626Mock(erc4626Mock));
-
-        // c = Contract(
-        //     create3.deploy(
-        //         getCreate3ContractSalt("Contract"),
-        //         bytes.concat(type(Contract).creationCode, abi.encode(param))
-        //     )
-        // );
+        streamHub = new ERC4626StreamHub(RemovableAssetsERC4626Mock(erc4626Mock));
 
         vm.stopBroadcast();
     }
