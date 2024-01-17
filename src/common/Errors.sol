537d8f455f5830c46a88e516a35db1cf3e9296fe
brkomir <nenad.milenkovic154@outlook.com>
2024-01-17 15:18:36 +0100

    move yield streaming functionality into a separate contract

---
+++ src/common/Errors.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity ^0.8.19;
+
+error TransferExceedsAllowance();
+error ZeroShares();
+error AddressZero();
+error CannotOpenStreamToSelf();
+error StreamDoesNotExist();
+error NoYieldToClaim();
+error LossToleranceExceeded();
