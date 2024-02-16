fc8e9ceb3276ec1dcb5b7ed233c8a529e9628fa7
brkomir <nenad.milenkovic154@outlook.com>
2024-02-16 15:22:19 +0100

    add some docs

--- src/YieldDCA.sol
+++ src/YieldDCA.sol
@@ -10,6 +10,24 @@ import {SafeERC20} from "openzeppelin-contracts/token/ERC20/utils/SafeERC20.sol"
 import {MockERC4626} from "solmate/test/utils/mocks/MockERC4626.sol";
 import {MockERC20} from "solmate/test/utils/mocks/MockERC20.sol";
 
+/**
+ * @title YieldDCA
+ * @dev This contract implements a Dollar Cost Averaging (DCA) strategy by utilizing yield generated from deposited ERC4626 tokens over fixed periods (epochs).
+ * The contract calculates the yield generated from the total principal deposited in every epoch, and sells the yield for a specified DCA token via the swapper contract.
+ * Distribution of the purchased DCA tokens among participants is based on their share of the total yield generated in each epoch.
+ *
+ * The contract tracks each user's deposit (in terms of shares of the vault and principal amount) and the epoch when the deposit was made.
+ * When users decide to withdraw, they receive their share of the DCA tokens bought during the epochs their deposit was active, adjusted for any yield generated on their principal.
+ *
+ * Key functionalities include:
+ * - Allowing users to deposit assets into a vault and participate in the DCA strategy.
+ * - Automatic progression through epochs, with the contract executing the DCA strategy by selling generated yield for DCA tokens at the end of each epoch.
+ * - Calculation of users' shares of DCA tokens based on the yield generated from their deposited principal.
+ * - Providing a mechanism for users to withdraw their original deposit and their share of DCA tokens generated from yields.
+ *
+ * This contract requires external integration with a vault (IERC4626 for asset management), a token to DCA into (IERC20), and a swapper contract for executing trades.
+ * It is designed for efficiency and scalability, with considerations for gas optimization and handling a large number of epochs and user deposits.
+ */
 contract YieldDCA {
     using FixedPointMathLib for uint256;
     using SafeERC20 for IERC4626;
@@ -35,7 +53,7 @@ contract YieldDCA {
 
     uint256 public currentEpoch = 1; // starts from 1
     uint256 public currentEpochTimestamp = block.timestamp;
-    uint256 public totalPrincipalDeposted;
+    uint256 public totalPrincipalDeposited;
     mapping(address => Deposit) public deposits;
     mapping(uint256 => EpochInfo) public epochDetails;
 
@@ -68,7 +86,7 @@ contract YieldDCA {
         position.principal += principal;
         position.epoch = currentEpoch;
 
-        totalPrincipalDeposted += principal;
+        totalPrincipalDeposited += principal;
     }
 
     function executeDCA() external {
@@ -82,9 +100,9 @@ contract YieldDCA {
         uint256 yield = vault.redeem(yieldInShares, address(this), address(this));
         // TODO: use asset.balanceOf here instead of yield?
 
-        uint256 realizedPricePerShare = yield.mulDivDown(1e18, yieldInShares);
+        uint256 realizedPricePerShare = yield.divWadDown(yieldInShares);
         uint256 tokensBought = _buyDcaToken(yield);
-        uint256 tokenPrice = tokensBought.mulDivDown(1e18, yield);
+        uint256 tokenPrice = tokensBought.divWadDown(yield);
 
         epochDetails[currentEpoch] =
             EpochInfo({yieldSpent: yield, dcaPrice: tokenPrice, pricePerShare: realizedPricePerShare});
@@ -95,7 +113,7 @@ contract YieldDCA {
 
     function calculateCurrentYieldInShares() public view returns (uint256) {
         uint256 balance = vault.balanceOf(address(this));
-        uint256 totalPrincipalInShares = vault.convertToShares(totalPrincipalDeposted);
+        uint256 totalPrincipalInShares = vault.convertToShares(totalPrincipalDeposited);
 
         return balance > totalPrincipalInShares ? balance - totalPrincipalInShares : 0;
     }
@@ -122,7 +140,7 @@ contract YieldDCA {
         dcaToken.safeTransfer(msg.sender, totalDcaTokensForUser);
 
         // update
-        totalPrincipalDeposted -= user.principal;
+        totalPrincipalDeposited -= user.principal;
 
         // update user position
         delete deposits[msg.sender];
