dcfa5f6a61dbcc489ed5f737809c7abec62d0b01
brkomir <nenad.milenkovic154@outlook.com>
2024-05-09 17:14:31 +0200

    consolidate YieldStreamsFactory contract

--- src/YieldStreamsFactory.sol
+++ src/YieldStreamsFactory.sol
@@ -1,15 +1,80 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity ^0.8.19;
 
-import {StreamsFactoryBase} from "./common/StreamsFactoryBase.sol";
+import {CREATE3} from "solmate/utils/CREATE3.sol";
+import {IERC4626} from "openzeppelin-contracts/interfaces/IERC4626.sol";
+
 import {YieldStreams} from "./YieldStreams.sol";
+import {CommonErrors} from "./common/CommonErrors.sol";
 
 /**
  * @title YieldStreamsFactory
- * @dev A contract for creating and managing YieldStreams instances.
+ * @notice Factory contract to deploy YieldStreams instances.
  */
-contract YieldStreamsFactory is StreamsFactoryBase {
-    function _getCreationCode(address _vault) internal pure override returns (bytes memory) {
-        return abi.encodePacked(type(YieldStreams).creationCode, abi.encode(_vault));
+contract YieldStreamsFactory {
+    using CommonErrors for address;
+
+    /**
+     * @dev Emitted when a new YieldStreams contract instance is deployed.
+     * @param caller The address of the caller of the create function.
+     * @param vault The address of the underlying ERC4626 vault contract.
+     * @param yieldStreams The address of the deployed YieldStreams contract instance.
+     */
+    event Deployed(address indexed caller, address vault, address yieldStreams);
+
+    error AlreadyDeployed();
+
+    /// @dev The addresses of deployed YieldStreams instances.
+    address[] public deployedAddresses;
+    /// @dev The number of deployed YieldStreams instances.
+    uint256 public deployedCount;
+
+    /**
+     * @dev Creates a new instance of the YieldStreams contract.
+     * @param _vault The address of the underlying ERC4626 vault contract.
+     * @return deployed The address of the deployed YieldStreams contract instance.
+     */
+    function create(IERC4626 _vault) public virtual returns (YieldStreams deployed) {
+        address(_vault).checkIsZero();
+
+        if (isDeployed(_vault)) revert AlreadyDeployed();
+
+        bytes memory creationCode = abi.encodePacked(type(YieldStreams).creationCode, abi.encode(_vault));
+
+        deployed = YieldStreams(CREATE3.deploy(getSalt(_vault), creationCode, 0));
+
+        deployedAddresses.push(address(deployed));
+
+        unchecked {
+            deployedCount++;
+        }
+
+        emit Deployed(msg.sender, address(_vault), address(deployed));
+    }
+
+    /**
+     * @dev Predicts the address of the deployed YieldStreams contract instance.
+     * @param _vault The address of the underlying ERC4626 vault contract.
+     * @return predicted The predicted address of the deployed YieldStreams contract instance.
+     */
+    function predictDeploy(IERC4626 _vault) public view returns (YieldStreams predicted) {
+        predicted = YieldStreams(CREATE3.getDeployed(getSalt(_vault), address(this)));
+    }
+
+    /**
+     * @dev Checks if a YieldStreams contract instance is deployed for the given ERC4626 vault address.
+     * @param _vault The address of the ERC4626 vault contract.
+     * @return bool Returns true if the YieldStreams contract instance is deployed, false otherwise.
+     */
+    function isDeployed(IERC4626 _vault) public view returns (bool) {
+        return address(predictDeploy(_vault)).code.length > 0;
+    }
+
+    /**
+     * @dev Returns the salt for the CREATE3 deployment.
+     * @param _vault The address of the underlying ERC4626 vault contract.
+     */
+    function getSalt(IERC4626 _vault) public pure returns (bytes32) {
+        return keccak256(abi.encode(_vault));
     }
 }
--- test/YieldStreamsFactory.t.sol
+++ test/YieldStreamsFactory.t.sol
@@ -2,23 +2,45 @@
 pragma solidity ^0.8.19;
 
 import "forge-std/Test.sol";
+import {IERC4626} from "openzeppelin-contracts/interfaces/IERC4626.sol";
 import {MockERC20} from "solmate/test/utils/mocks/MockERC20.sol";
 import {MockERC4626} from "solmate/test/utils/mocks/MockERC4626.sol";
 
-import {YieldStreamsFactory} from "../src/YieldStreamsFactory.sol";
-import {YieldStreams} from "../src/YieldStreams.sol";
+import {CommonErrors} from "src/common/CommonErrors.sol";
+import {YieldStreamsFactory} from "src/YieldStreamsFactory.sol";
+import {YieldStreams} from "src/YieldStreams.sol";
 
 contract YieldStreamsFactoryTest is Test {
-    function test_create_deploysYieldStreamsInstance() public {
-        MockERC20 asset = new MockERC20("ERC20Mock", "ERC20Mock", 18);
-        MockERC4626 vault = new MockERC4626(MockERC20(address(asset)), "ERC4626Mock", "ERC4626Mock");
+    YieldStreamsFactory public factory;
+    MockERC20 public asset;
+    IERC4626 public vault;
 
-        YieldStreamsFactory factory = new YieldStreamsFactory();
+    event Deployed(address indexed caller, address vault, address yieldStreams);
 
-        YieldStreams deployed = YieldStreams(factory.create(address(vault)));
+    function setUp() public {
+        factory = new YieldStreamsFactory();
 
-        assertEq(factory.deployedCount(), 1);
-        assertEq(factory.deployedAddresses(0), address(deployed));
+        asset = new MockERC20("Mock ERC20", "mERC20", 18);
+        vault = IERC4626(address(new MockERC4626(asset, "Mock ERC4626", "mERC4626")));
+    }
+
+    function test_create_failsIfVaultIsZero() public {
+        vm.expectRevert(CommonErrors.AddressZero.selector);
+        factory.create(IERC4626(address(0)));
+    }
+
+    function test_create_deploysYieldStreamsContract() public {
+        address predicted = address(factory.predictDeploy(vault));
+        assertTrue(!factory.isDeployed(vault), "isDeployed before");
+        assertEq(factory.deployedCount(), 0, "deployedCount before");
+
+        YieldStreams deployed = factory.create(vault);
+
+        assertTrue(address(deployed) != address(0));
+        assertTrue(factory.isDeployed(vault), "isDeployed after");
+        assertTrue(factory.deployedAddresses(0) == address(deployed), "deployedAddresses[0]");
+        assertEq(predicted, address(deployed), "predicted address");
+        assertEq(factory.deployedCount(), 1, "deployedCount");
 
         // assert vault is set
         assertEq(address(deployed.vault()), address(vault), "vault");
@@ -33,5 +55,42 @@ contract YieldStreamsFactoryTest is Test {
         deployed.open(receiver, shares, 0);
 
         assertEq(vault.balanceOf(address(deployed)), shares, "stream shares");
+    }
+
+    function test_create_emitsEvent() public {
+        address predicted = address(factory.predictDeploy(vault));
+
+        vm.expectEmit(true, true, true, true);
+        emit Deployed(address(this), address(vault), predicted);
+
+        address deployed = address(factory.create(vault));
+
+        assertEq(address(predicted), deployed, "predicted");
+    }
+
+    function test_create_failsIfAlreadyDepolyed() public {
+        factory.create(vault);
+
+        vm.expectRevert(YieldStreamsFactory.AlreadyDeployed.selector);
+        factory.create(vault);
+    }
+
+    function test_create_deployTwiceWorksForDifferentVaults() public {
+        MockERC20 asset2 = new MockERC20("ERC20Mock2", "ERC20Mock2", 18);
+        IERC4626 vault2 = IERC4626(address(new MockERC4626(asset2, "ERC4626Mock2", "ERC4626Mock2")));
+
+        address predicted = address(factory.predictDeploy(vault));
+        address predicted2 = address(factory.predictDeploy(vault2));
+
+        address deployed = address(factory.create(vault));
+        address deployed2 = address(factory.create(vault2));
+
+        assertTrue(factory.isDeployed(vault), "isDeployed");
+        assertTrue(factory.isDeployed(vault2), "isDeployed2");
+        assertTrue(factory.deployedAddresses(0) == deployed, "deployedAddresses[0]");
+        assertTrue(factory.deployedAddresses(1) == deployed2, "deployedAddresses[1]");
+        assertEq(predicted, deployed, "predicted");
+        assertEq(predicted2, deployed2, "predicted2");
+        assertEq(factory.deployedCount(), 2, "deployedCount");
     }
 }
